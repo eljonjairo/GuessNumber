4e4f7c8a7fcb208a0f152b098fbdcd77a4fe0606
John Diaz <104036407+eljonjairo@users.noreply.github.com>
2023-03-02 09:25:13 -0500

    last update check PEP8

--- GuessNumberComp.py
+++ GuessNumberComp.py
@@ -3,38 +3,40 @@
 # Guess the number (Computer) using an tkinter GUI
 # Based on FreeCodeCamp.org
 # John Diaz january 2023
-#TO DO List:
+# TO DO List:
 #
 
 import random
 from tkinter import *
 
 root = Tk()
-g = Entry(root, width=80, borderwidth=7, bg="white", fg="black")
-g.pack()
+root.option_add( "*font", "lucida 23 bold " )
+box = Entry(root, width=40, borderwidth=7, bg="white", fg="black")
+box.pack()
 
-
-# Initialize the guess number
-limit = random.randint(1,100)
-number = random.randint(1,limit)
+# Initialize the number to be guess
+limit = random.randint(10, 100)
+number = random.randint(1, limit)
 
 root.title(f" Enter a number between 0 and {limit}")
 
-def btnF():
-       # get the number in the box and substract with the actual number   
-        x = int(g.get()) - number
-        if x == 0:
-             btnLabel = Label(root, text=g.get() + " is the right number!!!")             
-        elif x > 0:
-             btnLabel = Label(root, text=g.get() + " is too high...")             
-        elif x < 0:
-             btnLabel = Label(root, text=g.get() + " is too low...")             
-        btnLabel.pack()        
 
-btn = Button(root, text=" Check you guess !!!", command=btnF)
+def btn_func():
+    # Get the number in the box and substract with the actual number
+    x = int(box.get()) - number
+    if x == 0:
+        btnLabel = Label(root, text=box.get() + " is the right number!!!")
+    elif x > 0:
+        btnLabel = Label(root, text=box.get() + " is too high...")
+    elif x < 0:
+        btnLabel = Label(root, text=box.get() + " is too low...")
+    btnLabel.pack()
+    # Clean the box
+    box.delete(0, END)
+
+
+btn = Button(root, text=" Check you guess !!!", command=btn_func)
 btn.pack()
-
-
 
 
 root.mainloop()
